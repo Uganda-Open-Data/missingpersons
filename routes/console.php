43647b726e33507b8c11d40ab386919fcd6df415
ssmusoke <ssmusoke@gmail.com>
2024-07-25 16:04:13 +0300

    Setup and Configure Laravel Backup
    1. Database every hour
    2. Files every 6 hours

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::command('emails:send Taylor --force')->daily();
+
+Schedule::command('backup:clean')->daily()->at('01:00');
+# backup the DB at 15 minutes past the hour
+Schedule::command('backup:run --only-db')->hourlyAt(15);
+# backup the files every 6 hours at 25 m
+Schedule::command('backup:run --only-files')->everySixHours(25);
