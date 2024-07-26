507aaa23179eaed2408549ff1fed45aff779a2f1
marshud <ubatamye@gmail.com>
2024-07-26 14:17:25 +0300

    reordering remanded fields

--- database/migrations/2024_07_25_160836_add_remand_and_release_fields_to_victim.php
+++ database/migrations/2024_07_25_160836_add_remand_and_release_fields_to_victim.php
@@ -14,14 +14,16 @@ return new class extends Migration
         Schema::table('victims', function (Blueprint $table) {
             //
             $table->unsignedBigInteger('remanded_from_id')->nullable();
+            $table->foreign('remanded_from_id')->references('id')->on('holding_locations'); // The Court
+
             $table->unsignedBigInteger('remanded_to_id')->nullable();
+            $table->foreign('remanded_to_id')->references('id')->on('holding_locations'); // The Prison
+            
             $table->date('remanded_on')->nullable();
             $table->date('remanded_until')->nullable();
             $table->text('remanded_by')->nullable(); // The Judge
             $table->date('released_on')->nullable();
 
-            $table->foreign('remanded_from_id')->references('id')->on('holding_locations'); // The Court
-            $table->foreign('remanded_to_id')->references('id')->on('holding_locations'); // The Prison
         });
     }
 
